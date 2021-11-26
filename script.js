f5d175cb7cc69175dd40e642af1b02d2cf4cca41
AnastasiyaHaidanovich <Nastyal0ve9517@gmail.com>
2021-11-26 14:25:23 +0300

    add: console.log using methods and properties

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-let title = "Project title";
-let screens = "Simple, Comprehensive, Interactive";
+let title = "Название проекта";
+let screens = "Простые, Сложные, Интерактивные";
 let screenPrice = 50;
 let rollback = 17;
 let fullPrice = 500;
 let adaptive = true;
 console.log("First console message");
-alert("Hi!");
+// alert("Hi!");
+
+console.log(typeof title, typeof fullPrice, typeof adaptive);
+console.log(screens.length);
+console.log("Стоимость верстки экранов " + screenPrice + " рублей");
+console.log("Стоимость разработки сайта " + fullPrice + " рублей");
+console.log(screens.toLowerCase().split(", "));
+console.log("Оплата посреднику: " + (fullPrice * (rollback/100)));
